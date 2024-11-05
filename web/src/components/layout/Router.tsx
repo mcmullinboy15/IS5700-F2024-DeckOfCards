dd5e35705694afe2a6cf3b8036ff4455d240e005
johniparker <a02359825@usu.edu>
2024-11-05 13:28:35 -0700

    added basic router structure

---
+++ web/src/components/layout/Router.tsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from 'react-router-dom';
+
+export const RouterProvider = () => {
+    return (
+        <Routes>
+            <Route path="/" element="" />
+        </Routes>
+    )
+}
