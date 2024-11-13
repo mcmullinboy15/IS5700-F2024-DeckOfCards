4470e1e5802f1e21bbb821e6568282ed0c5074de
Chance Wiese <chancewiese0925@gmail.com>
2024-11-13 16:41:53 -0700

    Add Login page with Google authentication

--- web/src/components/layout/Router.tsx
+++ web/src/components/layout/Router.tsx
@@ -1,10 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../../pages/HomePage";
+import Login from "../Login";
 
 export const RouterProvider = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-    </Routes>
-  );
+   return (
+      <Routes>
+         <Route path="/" element={<HomePage />} />
+         <Route path="/login" element={<Login />} />
+         <Route path="/register" element={<Login />} />
+      </Routes>
+   );
 };
