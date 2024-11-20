0b720ef3b7c94e1345560ba5d9d0e5368a6bdb3f
hamnerben <hamnerben@gmail.com>
2024-11-20 13:02:54 -0700

    got the player hand set up

--- web/src/components/BlackJackTable.tsx
+++ web/src/components/BlackJackTable.tsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box";
-import Card from "@mui/material/Card";
+import MuiCard from "@mui/material/Card";
+import { Typography } from "@mui/material";
 
 export default function BlackJackTable({}) {
   return (
@@ -18,19 +19,49 @@ export default function BlackJackTable({}) {
         table
         <Box sx={{ flexGrow: 1 }}>main area</Box>
         <Box
+          // {/* Player row */}
           id="player-row"
           sx={{
             bgcolor: "grey",
             bottom: 0,
             display: "flex",
             flexDirection: "row",
+            alignContent: "center",
+            justifyContent: "center",
+            padding: 2,
           }}
         >
-            <Box>
-                {/* PLAYER HAND */}
-                <Card>Here is a card</Card>
+          {/* PLAYER HAND */}
+          <Box
+            sx={{
+              bgcolor: "black",
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Typography sx={{ color: "red" }}>BUSTED</Typography>
+            <MuiCard
+              sx={{
+                width: "auto",
+                px: 1,
+                display: "flex",
+                justifyContent: "center",
+                margin: 1,
+              }}
+            >
+              bet
+            </MuiCard>
+
+            <Box
+              id="cards"
+              sx={{ display: "flex", flexDirection: "row", gap: 2 }}
+            >
+              <MuiCard sx={{ height: 150, width: 90 }}>Card 1</MuiCard>
+              <MuiCard sx={{ height: 150, width: 90 }}>Card 2</MuiCard>
             </Box>
-          player row
+          </Box>
         </Box>
       </Box>
     </>
