ecc2057aa4836518a119d8da95a356da40255a86
hamnerben <hamnerben@gmail.com>
2024-11-20 13:46:40 -0700

    made player hand component

--- web/src/components/BlackJackTable.tsx
+++ web/src/components/BlackJackTable.tsx
@@ -1,8 +1,15 @@
 import Box from "@mui/material/Box";
+import PlayerHand from "./PlayerHand";
 import MuiCard from "@mui/material/Card";
-import { Typography } from "@mui/material";
 
 export default function BlackJackTable({}) {
+
+  const cards1 = [<MuiCard sx={{ height: 150, width: 90 }}>Card 1</MuiCard>,
+  <MuiCard sx={{ height: 150, width: 90 }}>Card 2</MuiCard>]
+  const cards2 = [<MuiCard sx={{ height: 150, width: 90 }}>Card 3</MuiCard>,
+    <MuiCard sx={{ height: 150, width: 90 }}>Card 4</MuiCard>]
+    const cards3 = [<MuiCard sx={{ height: 150, width: 90 }}>Card 5</MuiCard>,
+      <MuiCard sx={{ height: 150, width: 90 }}>Card 6</MuiCard>]
   return (
     <>
       <Box
@@ -29,39 +36,12 @@ export default function BlackJackTable({}) {
             alignContent: "center",
             justifyContent: "center",
             padding: 2,
+            gap:2,
           }}
         >
-          {/* PLAYER HAND */}
-          <Box
-            sx={{
-              bgcolor: "black",
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-            }}
-          >
-            <Typography sx={{ color: "red" }}>BUSTED</Typography>
-            <MuiCard
-              sx={{
-                width: "auto",
-                px: 1,
-                display: "flex",
-                justifyContent: "center",
-                margin: 1,
-              }}
-            >
-              bet
-            </MuiCard>
-
-            <Box
-              id="cards"
-              sx={{ display: "flex", flexDirection: "row", gap: 2 }}
-            >
-              <MuiCard sx={{ height: 150, width: 90 }}>Card 1</MuiCard>
-              <MuiCard sx={{ height: 150, width: 90 }}>Card 2</MuiCard>
-            </Box>
-          </Box>
+          <PlayerHand busted={true} bet={40} cards={cards1}/>
+          <PlayerHand busted={false} bet={20} cards={cards2}/>
+          <PlayerHand busted={false} bet={30} cards={cards3} />
         </Box>
       </Box>
     </>
---
+++ web/src/components/PlayerHand.tsx
@@ -0,0 +1,47 @@
+import MuiCard from "@mui/material/Card";
+import { Typography } from "@mui/material";
+import Box from "@mui/material/Box";
+// import type {Card} from "../game-logic/api"
+
+interface PlayerHandProps {
+  busted: boolean;
+  bet: number;
+  cards: JSX.Element[];
+}
+
+export default function PlayerHand({ busted, bet, cards }: PlayerHandProps) {
+  return (
+    <>
+      {/* PLAYER HAND */}
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {busted ? (
+          <Typography variant="h5" sx={{ color: "red" }}>
+            BUSTED
+          </Typography>
+        ) : null}
+        <MuiCard
+          sx={{
+            width: "auto",
+            px: 1,
+            display: "flex",
+            justifyContent: "center",
+            margin: 1,
+          }}
+        >
+          <Typography sx={{font:"roboto"}}>{bet}</Typography>
+        </MuiCard>
+
+        <Box id="cards" sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
+          {cards}
+        </Box>
+      </Box>
+    </>
+  );
+}
