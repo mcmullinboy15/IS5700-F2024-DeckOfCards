06b490aaa0b9c2c8443b4f398ca0fd9291ef530e
Andrew McMullin <mcmullinboy15@gmail.com>
2024-11-21 09:32:33 -0700

    Merged main

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,15 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
-import AuthProvider from "./context/AuthContext.tsx";
-import ChatComponent from "./components/ChatComponent.tsx";
+import App from "./App";
+import AuthProvider from "./context/AuthContext";
+import ChatComponent from "./components/ChatComponent";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <AuthProvider>
       <App />
     </AuthProvider>
-    <ChatComponent chatName="Global Chat" />
+    <ChatComponent />
+    {/* <ChatComponent chatName="Global Chat" /> */}
   </StrictMode>
 );
